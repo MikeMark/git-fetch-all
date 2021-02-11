0f4e841e231a1a7507e75a1338a8e01d52705f50
Jonathan Markovsky <jon.markov@icloud.com>
2021-02-10 20:02:10 -0700

    Added first run comm

--- git-copy-all.py
+++ git-copy-all.py
@@ -4,3 +4,7 @@
 # created by Jonathan Markovsky, jon.markov@icloud.com, 2021/02/10
 #
 
+import subprocess
+
+cpr = subprocess.run(['echo','hello world'], capture_output=True, text=True)
+print(cpr.stdout)
